e2d7acb1dcd05d7f3849469333f628344df0f7e3
masa101224 <masa101224@outlook.jp>
2025-08-21 17:55:32 +0900

    通常弾アニメーション

--- Assets/Script/MainTriger/AsteroidBullet.cs
+++ Assets/Script/MainTriger/AsteroidBullet.cs
@@ -8,13 +8,20 @@ public class AsteroidBullet : MonoBehaviour
     private float speed;
     private Vector3 direction;
     public float lifeTime = 1f;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     public void Setup(Vector3 dir,float spd,int dmg)
     {
-        direction = dir;
+        direction = dir.normalized;
         speed = spd;
         damage = dmg;
         Destroy(gameObject, lifeTime);
+        directionBullet();
     }
 
     // Update is called once per frame
@@ -27,10 +34,17 @@ public class AsteroidBullet : MonoBehaviour
     {
         if (other.CompareTag("Enemy"))
         {
-            Debug.Log("Hit Boss");
-
             other.GetComponent<Enemy>()?.TakeDamage(damage);
             Destroy(gameObject);
+        }
+    }
+
+    void directionBullet()
+    {
+        if(direction != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(direction.y,direction.x) * Mathf.Rad2Deg + 180f; 
+            transform.rotation = Quaternion.Euler(0f, 0f, angle);
         }
     }
 }
